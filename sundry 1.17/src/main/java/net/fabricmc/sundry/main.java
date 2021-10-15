4f40b5d0fcd41e1449037f4b53fbc303479929cd
Traumatization <82266244+Traumatization@users.noreply.github.com>
2021-10-15 21:40:32 +0200

    Add files via upload

--- sundry 1.17/src/main/java/net/fabricmc/sundry/main.java
+++ sundry 1.17/src/main/java/net/fabricmc/sundry/main.java
@@ -1,14 +1,21 @@
 package net.fabricmc.sundry;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class main implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
+	public static final String MOD_ID = "sundry";
+
 	public static final Logger LOGGER = LogManager.getLogger("modid");
+
+	public static final Item SALT = new Item(new FabricItemSettings().group(ItemGroup.FOOD));
 
 	@Override
 	public void onInitialize() {
@@ -17,5 +24,10 @@ public class main implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		net.minecraft.util.registry.Registry.register(Registry.ITEM, new Identifier("sundry", "new_item"), SALT);
+
+
+
 	}
 }
